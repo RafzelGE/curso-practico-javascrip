a502d673cc03ecefb217e5ef78db173f6ff081d9
Rafael Gonzalez <rafaelgonzalezescalona@gmail.com>
2023-07-06 20:10:20 -0400

    V1.0 de la pag de practica

--- main.js
+++ main.js
@@ -3,33 +3,51 @@ const menu = document.querySelector('.menu');
 const email = document.querySelector('.navEmail');
 const mobileMenu = document.querySelector('.mobile-menu');
 const navShopping = document.querySelector('.navShopping');
-const aside = document.querySelector('.product-detail');
+const asideShopping = document.querySelector('.product-detail');
 const cardsContainer = document.querySelector('.cards-container');
+const asideInfo = document.querySelector('.product-detail-info');
+const productDetailClose = document.querySelector('.product-detail-close');
+
 
 navShopping.addEventListener('click', toggleShoppingCart);
 email.addEventListener('click', toggleMenu);
 menu.addEventListener('click', toggleMobileMenu);
+productDetailClose.addEventListener('click', closeDetail);
+function closeDetail() {
+    const isAsideInfoClosed = asideInfo.classList.contains('inactive');
+    if (!isAsideInfoClosed) {
+        asideInfo.classList.add('inactive');
+    }
+}   
 
 function toggleMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = asideShopping.classList.contains('inactive');
+    const isAsideInfoClosed = asideInfo.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        asideShopping.classList.add('inactive');
+    }
+    if (!isAsideInfoClosed) {
+        asideInfo.classList.add('inactive')
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
-
+    const isAsideClosed = asideShopping.classList.contains('inactive');
+    const isAsideInfoClosed = asideInfo.classList.contains('inactive');
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        asideShopping.classList.add('inactive');
+    }
+    if (!isAsideInfoClosed) {
+        asideInfo.classList.add('inactive')
     }
     mobileMenu.classList.toggle('inactive');
 }
 function toggleShoppingCart() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isAsideInfoClosed = asideInfo.classList.contains('inactive');
 
     if (!isDesktopMenuClosed) {
         desktopMenu.classList.add('inactive')
@@ -38,9 +56,17 @@ function toggleShoppingCart() {
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive');
+    if (!isAsideInfoClosed) {
+        asideInfo.classList.add('inactive')
+    }
+    asideShopping.classList.toggle('inactive');
 }
-
+function closeDetail() {
+    const isAsideInfoClosed = asideInfo.classList.contains('inactive');
+    if (!isAsideInfoClosed) {
+        asideInfo.classList.add('inactive');
+    }
+}
 const productList = [];
 productList.push({
     name: 'bike',
@@ -57,58 +83,83 @@ productList.push({
     price: 120,
     img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 })
-
-/*
-<div div class="product-card">
-    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        alt="">
-    <div class="product-info">
-        <div>
-            <p>$120,00</p>
-            <p>Bike</p>
-        </div>
-        <figure>
-            <img src="https://static.platzi.com/media/tmp/class-files/github/curso-frontend-developer-javascript-practico/curso-frontend-developer-javascript-practico-11c672c1facc262ea3c11ecc8d95e1f0b0e67a0b/icons/bt_add_to_cart.svg" alt="">
-        </figure>
-    </div>
-</div>
-*/
-
-
+productList.push({
+    name: 'teclado',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name: 'raton',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
 function renderProducts(array) {
-    for (product of productList) {
-        const productCard = document.createElement('div');
+    for (product of array) {
+        
+    const productCard = document.createElement('div');
+
+    const productImg = document.createElement('img');
+
+    const productInfo = document.createElement('div');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+
+    const productName = document.createElement('p');
+
+    const productInfoFigure = document.createElement('figure');
+
+    const productImgCart = document.createElement('img');
+        console.log(product);
         productCard.classList.add('product-card');
-    
-        const productImg = document.createElement('img');
+
         productImg.setAttribute('src', product.img);
-    
-        const productInfo = document.createElement('div');
+
         productInfo.classList.add('product-info');
-    
-        const productInfoDiv = document.createElement('div');
-    
-        const productPrice = document.createElement('p');
+
+
         productPrice.innerText = '$' + product.price;
-    
-        const productName = document.createElement('p');
+
         productName.innerText = product.name;
-    
+
         productInfoDiv.appendChild(productPrice);
         productInfoDiv.appendChild(productName);
-    
-        const productInfoFigure = document.createElement('figure');
-        const productImgCart = document.createElement('img');
+
         productImgCart.setAttribute('src', 'https://static.platzi.com/media/tmp/class-files/github/curso-frontend-developer-javascript-practico/curso-frontend-developer-javascript-practico-11c672c1facc262ea3c11ecc8d95e1f0b0e67a0b/icons/bt_add_to_cart.svg');
-    
+
         productInfoFigure.appendChild(productImgCart);
         productInfo.appendChild(productInfoDiv);
         productInfo.appendChild(productInfoFigure);
-    
+
         productCard.appendChild(productImg);
         productCard.appendChild(productInfo);
-    
+
         cardsContainer.appendChild(productCard);
+
+        productCard.addEventListener('click', openInfo);
+
+        function openInfo() {
+            const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+            const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+            const isAsideClosed = asideShopping.classList.contains('inactive');
+            const isAsideInfoClosed = asideInfo.classList.contains('inactive');
+    
+            if (!isDesktopMenuClosed) {
+                desktopMenu.classList.add('inactive')
+            }
+    
+            if (!isMobileMenuClosed) {
+                mobileMenu.classList.add('inactive');
+            }
+            if (!isAsideClosed) {
+                asideShopping.classList.add('inactive')
+            }
+            if (!isAsideInfoClosed) {
+                asideInfo.classList.add('inactive');
+            }
+            asideInfo.classList.remove('inactive');
+        }
     }
 }
 
